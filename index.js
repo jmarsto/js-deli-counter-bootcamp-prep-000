e918081b5d8e9c30c69ee7fb4d6e35efc735cf90
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-02-16 20:29:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ function takeANumber(katzDeliLine, newName) {
   return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing() {
+function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     return `Currently serving ${katzDeliLine[0]}.`
     katzDeliLine.shift()
   }
   else {
-        return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!"
   }
 }
